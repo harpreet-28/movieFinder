cc13091020538a26509428abfdb92b6466415606
preet.k.k88@gmail.com <preetkaur@Preets-MacBook-Pro.local>
2017-11-13 16:22:37 -0500

    issues with syntax :bow:

---
+++ src/js/components/App.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStrore = require('../stores/AppStore')
+var ReactDOM = require('react-dom');
+
+var App = React.creatClass(){
+	render: function(){
+		return(
+			<div>This is heading One </div>
+			)
+	}
+}
+module.export = App;
